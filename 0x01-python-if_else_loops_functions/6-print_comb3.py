52b680e9675ec4f3d30f4095fb082fa365d31906
55Nights <1905587@students.kca.ac.ke>
2022-06-29 12:42:13 +0000

    combine

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 x = range(0, 10)
 for i in x:
     for j in range(i + 1, 10):
-        if x == 8 and j == 9:
+        if i == 8 and j == 9:
             print("{}{}" .format(i, j))
         else:
             print("{}{}" .format(i, j), end=', ')
