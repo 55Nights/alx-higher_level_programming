8883579f02ccd93e9c5c7fd450e9dbc1e0ff1c70
55Nights <1905587@students.kca.ac.ke>
2022-06-29 16:06:31 +0000

    Fizz Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,7 +4,7 @@ def fizzbuzz():
     r = range(1, 101)
     for i in r:
         if i % 3 == 0 and i % 5 == 0:
-            print("{}" .format("Fizz Buzz"), end=' ')
+            print("{}" .format("FizzBuzz"), end=' ')
         elif i % 3 == 0:
             print("{}" .format("Fizz"), end=' ')
         elif i % 5 == 0:
