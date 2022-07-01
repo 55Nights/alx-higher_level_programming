3fe433d781c4d90946e6d3772879e9fd77b38c82
55Nights <1905587@students.kca.ac.ke>
2022-07-01 09:27:52 +0000

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 import sys
-x = len(sys.argv) - 1
-if x == 0:
-    print("{} arguments." .format(x))
-elif x == 1:
-    print("{} argument:" .format(x))
-else:
-    print("{} arguments:" .format(x))
-y = sys.argv
-z = 1
-while z <= x:
-    print("{}: {}" .format(z, y[z]))
-    z += 1
+
+if __name__ == "__main__":
+    x = len(sys.argv) - 1
+    if x == 0:
+        print("{} arguments." .format(x))
+    elif x == 1:
+        print("{} argument:" .format(x))
+    else:
+        print("{} arguments:" .format(x))
+        y = sys.argv
+        z = 1
+        while z <= x:
+            print("{}: {}" .format(z, y[z]))
+            z += 1
