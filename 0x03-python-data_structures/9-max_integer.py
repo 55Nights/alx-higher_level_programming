feb9cbefd4d4bff3d967fcd3d43b289857b72092
55Nights <1905587@students.kca.ac.ke>
2022-07-05 08:21:31 +0000

    max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    my_list = list(sorted(my_list, reverse=True))
-    return my_list[0]
+    if len(my_list) == 0:
+        return None
+    else:
+        my_list = list(sorted(my_list, reverse=True))
+        return my_list[0]
