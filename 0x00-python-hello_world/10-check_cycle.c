7b103d70aa14c2fad91676c8a66e2bf5023f7b27
55Nights <1905587@students.kca.ac.ke>
2022-07-16 13:51:38 +0300

    linked list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if there is a cycle in a list
+ *
+ * @list: the list to check
+ *
+ * Return: 0 or 1
+ *
+ */
+
+int check_cycle(listint_t *list)
+{
+	if (list == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
