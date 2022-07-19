f51c302283da7545bdbb23024de5a97a2a625d80
55Nights <1905587@students.kca.ac.ke>
2022-07-19 08:00:57 +0300

    exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,14 @@ def safe_print_list(my_list=[], x=0):
         while i < x:
             print("{}".format(my_list[i]), end='')
             i += 1
+
+        return x
     except BaseException:
-        print("something went wrong")
+        y = 0
+        for i in my_list:
+            y += 1
+
+        return y
+
     finally:
         print()
