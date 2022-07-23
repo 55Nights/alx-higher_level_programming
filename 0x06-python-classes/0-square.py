d41cd011127724eb715effd63dcdee753e07bd0c
55Nights <1905587@students.kca.ac.ke>
2022-07-23 19:59:33 +0300

    my first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-
+""" Defines a class square"""
 class Square(object):
-    """ 
-    defines a class square
-    Args: has no arguments still
-    """
+    """empty class square"""
     pass
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+class Square:
+
+    def __init__(self, size=0):
+        try:
+            if isinstance(size, int) is not True:
+                raise TypeError
+            elif size < 0:
+                raise ValueError
+            else:
+                self.__size = size
+        except TypeError:
+            print("size must be an integer")
+        except ValueError:
+            print("size must be >= 0")
+
+    def area(self):
+        """ calculates the area of a square """
+        return (self.__size * self.__size)
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, value):
+        self.__size = value
