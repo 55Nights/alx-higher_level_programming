8fb53137c6498d2547216774de8c9cd0a531cdce
55Nights <1905587@students.kca.ac.ke>
2022-07-26 10:01:54 +0300

    Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+""" class with private attr"""
+
+
+class Square:
+
+    """ init method """
+    def __init__(self, size=0):
+        """The constructor of class"""
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+
+        """  returns the current square area """
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+
+        """ This is a getter method """
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ This is the setter method """
+        if (type(value) is not int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """ prints in stdout the square with the character #:"""
+        s = self.__size
+        if s == 0:
+            print()
+        else:
+            for i in range(s):
+                for x in range(s):
+                    print("{}".format('#'), end='')
+                print()
