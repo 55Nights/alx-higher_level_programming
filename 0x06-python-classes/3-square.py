29e81073475345d955ecc30b14dea474dc589b14
55Nights <1905587@students.kca.ac.ke>
2022-07-26 09:40:16 +0300

    Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 
+""" class with private attr"""
+
+
 class Square:
 
+    """ init method """
     def __init__(self, size=0):
-        try:
-            if isinstance(size, int) is not True:
-                raise TypeError
-            elif size < 0:
-                raise ValueError
-            else:
-                self.__size = size
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
-
+        """The constructor of class"""
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
     def area(self):
-        """ calculates the area of a square """
+        """  returns the current square area """
         return (self.__size * self.__size)
